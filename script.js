95ff4563cee5c6be3b7c4b0b267225ed88ada99d
yanellaft <yanellaft@gmail.com>
2025-12-19 21:57:52 -0500

    added personalization CLICKING ON ORNAMENTS DOES NOT WORK

--- script.js
+++ script.js
@@ -2,33 +2,44 @@ const date = new Date();
 const days = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
-
-const ornamentImg = [];
-const stars = [];
+//"1.png", "2.png",
+const ornamentImg = ["🎁", "🪩", "⚜️", "🦌", "🔔", "🎅", "☃️", "🍪", "💝", "🔴", "🎊", "💎", "🏮", "🏵", "🧸", "☕", "❣️", "🛷"];
+const stars = ["⭐", "🌟", "✶", "✨", "★", "💫", "✯", "✪", "✴︎"];
 
 function changeStar() {
-    let randomIndex = Math.floor(Math.random() * 6);
+    let randomIndex = Math.floor(Math.random() * 9);
     document.getElementById("star").innerText = stars[randomIndex];
 }
 
+function changeOrnament(id) {
+    let ranIndex = Math.floor(Math.random() * 18);
+    document.getElementById("id").innerText = ornamentImg[ranIndex];
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    //const decorations = document.getElementById("decorations");
+    const decorations = document.getElementById("decorations");
     const dateText = document.getElementById("date");
     const star = document.getElementById("star");
 
-    dateText.innerText = days + "/" + month + "/" + year;
+    dateText.innerText = month + "/" + days + "/" + year;
 
-    if (month === 12) {
+    /*if (month === 12) {
         let daysTilChristmas = Math.max(25 - days, 0);
 
         for (let i = 0; i < daysTilChristmas; i++) {
-            const randomImg = ornamentImg[Math.floor(Math.random() * 18)]
-            //decorations.innerHTML +=
-            const ornament = document.getElementById("ornament" + i);
-            ornament.style.marginLeft = (Math.random()*10+4)+"rem";
-            ornament.style.marginTop = (Math.random()*2+3)+"rem";
+            const randomImg = ornamentImg[Math.floor(Math.random() * 2)]
+            decorations.innerHTML += `<img src="img/orn/${randomImg}" class="ornament" id="ornament${i}">`;            //const ornament = document.getElementById("ornament" + i);
+            const ornament = document.getElementById("ornament"+i);
+            ornament.style.width = (Math.random() * 1.7 + 1) + "rem";
+            ornament.style.left = (Math.random() * 40 + 30) + "%"
+            ornament.style.top = (Math.random() * 10 + 6) + "rem"
         }
-    }
+    }*/
 
     star.addEventListener('click', changeStar);
+    //ornament.addEventListener('click', changeOrnament);
+    for (let i = 1; i <= 5; i++) {
+        const ornament = document.getElementById(`ornament${i}`);
+        ornament.addEventListener('click', () => changeOrnament(`ornament${i}`));
+    }
 })
